f60525a25334b4bb2c6036ecffbd7d4aede5f325
Makcy <644215038@qq.com>
2019-12-31 12:30:09 +0800

    1.0.0

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,21 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+const request = require('request-promise');
 
+const URL = 'https://www.toutiao.com/stream/widget/local_weather/data';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "Tressa" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+export async function activate(context: vscode.ExtensionContext) {
+	const barItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+	try {
+		const response = await request({url: URL, json: true});
+		const {city, weather:{day_condition, high_temperature, low_temperature}} = response.data;
+		barItem.text = `${city} ${day_condition} ${low_temperature}°c ~ ${high_temperature}°c `;
+	} catch (error) {
+		barItem.text = '获取天气失败';
+	}
+	barItem.show();
 }
 
 // this method is called when your extension is deactivated
